25616fd6f039b50b9d805c5a2ae1bee1abfee198
Reizzel <cbp453252.hdrl@gmail.com>
2017-10-02 20:57:59 -0500

    Inicio Proyecto

---
+++ Hidato/src/hidato/Nodo.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hidato;
+
+/**
+ *
+ * @author lis
+ */
+public class Nodo {
+    private int valor;
+    private int fila;
+    private int columna;
+    private boolean visitado;
+
+    public Nodo(int numero, int fila, int columna) {
+        this.valor = numero;
+        this.fila = fila;
+        this.columna = columna;
+    }
+    
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+    
+    public int getNumero() {
+        return valor;
+    }
+
+    public void setNumero(int numero) {
+        this.valor = numero;
+    }
+
+    public boolean isVisitado() {
+        return visitado;
+    }
+
+    public void setVisitado(boolean visitado) {
+        this.visitado = visitado;
+    }
+    
+    
+    
+}
